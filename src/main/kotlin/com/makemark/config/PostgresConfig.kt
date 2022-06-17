2623c89936a0008a981799847e9215fb5a53694f
waccess.krivtsov <O;O$'LMMRt7^GDjH1M,=>
2022-06-17 13:19:22 +0300

    MMARK: added redis

--- src/main/kotlin/com/makemark/config/PostgresConfig.kt
+++ src/main/kotlin/com/makemark/config/PostgresConfig.kt
@@ -4,12 +4,10 @@ import com.github.jasync.sql.db.ConnectionPoolConfiguration
 import com.github.jasync.sql.db.SuspendingConnection
 import com.github.jasync.sql.db.asSuspending
 import com.github.jasync.sql.db.pool.ConnectionPool
-import com.github.jasync.sql.db.pool.PoolConfiguration
 import com.github.jasync.sql.db.postgresql.pool.PostgreSQLConnectionFactory
 import com.makemark.config.property.PostgresProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import java.util.concurrent.TimeUnit
 import com.github.jasync.sql.db.Configuration as JasyncConfiguration
 
 @Configuration(proxyBeanMethods = false)
@@ -40,13 +38,16 @@ class PostgresConfig(
     )
 
     private val poolConfiguration = ConnectionPoolConfiguration(
-//        maxObjects = 100,
-//        maxIdle = TimeUnit.MINUTES.toMillis(15),
-//        maxQueueSize = 10_000,
-//        validationInterval = TimeUnit.SECONDS.toMillis(30)
+        maxActiveConnections = postgresProperty.maxActiveConnections,
+//        maxPendingQueries = postgresProperty.maxPendingQueries,
+        maxConnectionTtl = postgresProperty.maxConnectionTtl,
+        maxIdleTime = postgresProperty.maxIdleTime
     )
 
     @Bean
-    fun pool() : SuspendingConnection =
-        ConnectionPool(factory = PostgreSQLConnectionFactory(configuration), configuration = poolConfiguration).asSuspending
+    fun pool(): SuspendingConnection =
+        ConnectionPool(
+            factory = PostgreSQLConnectionFactory(configuration),
+            configuration = poolConfiguration
+        ).asSuspending
 }
