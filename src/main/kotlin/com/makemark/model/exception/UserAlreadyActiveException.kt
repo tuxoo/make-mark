c4ad01c78b0cbfdb0677a000fd8267c4f0e2979a
waccess.krivtsov <O;O$'LMMRt7^GDjH1M,=>
2022-06-28 14:13:19 +0300

    MMARK: removed a check;

--- src/main/kotlin/com/makemark/model/exception/UserAlreadyActiveException.kt
+++
@@ -1,7 +0,0 @@
-package com.makemark.model.exception
-
-import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.ResponseStatus
-
-@ResponseStatus(HttpStatus.BAD_REQUEST)
-class UserAlreadyActiveException(message: String?) : RuntimeException(message)
